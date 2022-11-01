400b33d48137200b65aa18b2d11aedf0519bf140
Jinsung <46344256+stsoul47@users.noreply.github.com>
2022-11-01 21:43:40 +0900

    [feat] 절대경로 추가
    [feat] 절대경로 추가

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = (env, argv) => {
       alias: {
         '@': path.resolve(__dirname, 'src/'),
         '@assets': path.resolve(__dirname, 'src/assets/'),
+        '@components': path.resolve(__dirname, 'src/components/'),
+        '@pages': path.resolve(__dirname, 'src/pages/'),
+        '@hooks': path.resolve(__dirname, 'src/hooks/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
+        '@store': path.resolve(__dirname, 'src/store'),
       }
     },
     module: {
