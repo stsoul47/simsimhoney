e757be4b5b25a12bbaf220193e932e6511674fc4
Jinsung <46344256+stsoul47@users.noreply.github.com>
2022-11-11 19:02:19 +0900

    [feat] redux settings
    [feat] redux settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import rootReducer from "./redux";
+import { legacy_createStore as createStore } from 'redux';
 import App from './App';
 
 import '@assets/css/reset.css'
-import {BrowserRouter} from "react-router-dom";
+
+//store 설정
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,12 @@
+import {useSelector} from "react-redux";
+import { RootState, TestState } from "../../redux";
+
 const Main = () => {
+  const test:TestState = useSelector((state:RootState) => state.test);
   return (
     <div>
       여기가 메인 페이지 입니다.
+      {test.status}
     </div>
   );
 }
