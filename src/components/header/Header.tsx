97ff5a16cd5d7176526bb1ae4c99a1d8a6e20588
Jinsung <46344256+stsoul47@users.noreply.github.com>
2022-11-12 22:52:01 +0900

    [feat] layout (header)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import {Link, useLocation} from "react-router-dom";
+import classNames from "classnames";
 
 const Header = (props: {menu:{id: number, category: string, title: string, link: string, element: JSX.Element}[]}) => {
   const location = useLocation();
   return (
     <div className="app-header">
-      <ul>
+      <div className={classNames('header-left')}></div>
+      <ul className={classNames('nav')}>
         {props.menu.map((res:any)=>
           <li key={res.id}>
             <Link
+              className={classNames('header-link')}
               to={res.link}
             >{res.title}
             </Link>
